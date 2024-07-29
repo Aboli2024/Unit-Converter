58f003e62de4b58be126f833415b6b53eb8b7e9b
Aboli2024 <144131563+Aboli2024@users.noreply.github.com>
2024-07-29 21:57:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,18 @@
-package com.example.myquizapplication;
+package com.example.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    TextView totalQuestionsTextView;
-    TextView questionTextView;
-    Button ansA,ansB,ansC,ansD;
-    Button submitBtn;
-
-    int score=0;
-    int totalQuestion = QuestionAnswer.question.length;
-    int currentQuestionIndex = 0;
-    String selectedAnswer = "";
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView2;
+    private EditText editText;
 
 
 
@@ -27,91 +20,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        totalQuestionsTextView = findViewById(R.id.total_question);
-        questionTextView = findViewById(R.id.question);
-        ansA = findViewById(R.id.ans_A);
-        ansB = findViewById(R.id.ans_B);
-        ansC = findViewById(R.id.ans_C);
-        ansD = findViewById(R.id.ans_D);
-        submitBtn = findViewById(R.id.submit_btn);
-
-        ansA.setOnClickListener(this);
-        ansB.setOnClickListener(this);
-        ansC.setOnClickListener(this);
-        ansD.setOnClickListener(this);
-        submitBtn.setOnClickListener(this);
-
-        totalQuestionsTextView.setText("total questions : "+totalQuestion);
-
-        loadNewQuestion();
-
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        ansA.setBackgroundColor(Color.WHITE);
-        ansB.setBackgroundColor(Color.WHITE);
-        ansC.setBackgroundColor(Color.WHITE);
-        ansD.setBackgroundColor(Color.WHITE);
-
-
-        Button clickedButton = (Button) view;
-        if (clickedButton.getId()==R.id.submit_btn){
-
-            if (selectedAnswer.equals(QuestionAnswer.correctAnswer[currentQuestionIndex])){
-                score++;
+        button = findViewById(R.id.button);
+        textView2 = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.20462 * kg;
+                textView2.setText("The Pound Value is" + pound);
+                Toast.makeText(MainActivity.this, "The Vale is" + pound, Toast.LENGTH_SHORT).show();
             }
-            currentQuestionIndex++;
-            loadNewQuestion();
+        });
 
-
-        }else{
-            //choices  button clicked
-            selectedAnswer = clickedButton.getText().toString();
-            clickedButton.setBackgroundColor(Color.MAGENTA);
-        }
-
-
-    }
-
-    void loadNewQuestion(){
-
-        if (currentQuestionIndex == totalQuestion){
-            finishQuiz();
-            return;
-        }
-
-        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
-        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
-        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
-        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
-        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
-
-    }
-
-    void finishQuiz(){
-        String passStatus = "";
-        if (score > totalQuestion*0.60){
-            passStatus = "Passed";
-        }else{
-            passStatus = "Failed";
-        }
-        new AlertDialog.Builder(this)
-                .setTitle(passStatus)
-                .setMessage("Score is"+ score+" out of "+ totalQuestion)
-                .setPositiveButton("Restart",(dialogInterface, i)-> restartQuiz() )
-                .setCancelable(false)
-                .show();
-
-    }
-
-    void restartQuiz(){
-        score = 0;
-        currentQuestionIndex = 0;
-        loadNewQuestion();
 
     }
 }
